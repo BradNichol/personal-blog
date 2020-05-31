ccde542d085e9d0a3ac1f04f416a734fe3a10d08
Bradley Nichol <bradleynichol@live.co.uk>
2020-05-31 22:38:00 +0100

    material-ui cards import

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,5 +1,9 @@
 import React from "react"
 import Carousel from "react-material-ui-carousel";
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
 
 function CarouselSlider() {
   return <></>
