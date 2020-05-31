d763957dc790f85f152dbc4345cb0f6364c63b00
Bradley Nichol <bradleynichol@live.co.uk>
2020-05-31 23:04:16 +0100

    test data added, with state, carousel and card

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,12 +1,35 @@
-import React from "react"
-import Carousel from "react-material-ui-carousel";
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
+import React, { useState } from "react"
+import Carousel from "react-material-ui-carousel"
+import Card from "@material-ui/core/Card"
+//import CardActions from "@material-ui/core/CardActions"
+import CardContent from "@material-ui/core/CardContent"
+// import Button from "@material-ui/core/Button"
 
 function CarouselSlider() {
-  return <></>
+  const [items, setItems] = useState([
+      {
+          title: 'Personal blog',
+          lastUpdated: "05-05-2020"
+      },
+      {
+        title: 'SharedBread',
+        lastUpdated: "028-05-2020"
+    }
+    ])
+
+  return (
+      <Carousel>
+          {items.map(item => {
+              return (
+                  <Card>
+                      <CardContent>
+                          {item.title}
+                      </CardContent>
+                  </Card>
+              )
+          })}
+      </Carousel>
+  )
 }
 
 export default CarouselSlider
