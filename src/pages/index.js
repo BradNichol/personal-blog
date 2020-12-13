9805c6ae71e380b11b403b47dc05750aa259a8a3
Bradley Nichol <bradleynichol@live.co.uk>
2020-12-13 18:40:52 +0000

    Changes bg colour and font sizes. Updated hompage information

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,18 +8,31 @@ import Carousel from "../components/carousel";
 
 class IndexPage extends React.Component {
   render() {
-    const siteTitle = "Bradley Nichol"
+    const siteTitle = " Bradley Nichol"
 
     return (
-      <Layout location={this.props.location} title={siteTitle}>
+      <Layout location={this.props.location}>
         <SEO
           title={siteTitle}
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
+        <h1>Hi, I'm Bradley Nichol</h1>
         
-        <h2>Software Developer</h2>
-        <p>Welcome to my personal blog which is
-          currently under development. Check back  soon.
+        <p>I'm a software developer based in Leyland, Lancashire,
+          working for a FTSE100 company in the automotive industry. I predominately
+          work on backend systems using Java, with a sprinkle of frontend Angular work for 
+          internal tools.
+        </p>
+        <p>
+          In my past life I was a digital marketing professional for many years, and after
+          nearly two years of self-development I successfully transitioned into a software
+          development career. 
+        </p>
+        <p>
+          I'm sharing my thoughts, journey and whatever I'm learning here. 
+        </p>
+        <p>
+          Thanks for stopping by.
         </p>
         <Link to="/blog/">
           <Button marginTop="35px">View Blog Posts</Button>
